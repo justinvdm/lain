f06c259661b2ac9cc00933d69ae7502d399ae573
justinvdm <justinvdm.0.1.0@gmail.com>
2013-11-23 21:35:04 +0200

    Get overtone 0.8.1 working with clojure 1.5.1 (seesaw dep was making clojure 1.3.0 get used)

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Serial experiments with overtone"
   :url "https://github.com/justinvdm/lain"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [overtone "0.8.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone "0.8.1" :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[speclj "2.7.5"]]}}
   :plugins [[speclj "2.7.5"]]
   :test-paths ["test"])

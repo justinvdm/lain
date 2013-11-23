1b5b21bbac6aaca03dfbacacfaa2c9c6ce20079c
justinvdm <justinvdm.0.1.0@gmail.com>
2013-11-23 15:05:38 +0200

    Add a midi key player (with bending!)

---
+++ src/overtone_roland_a300/midi.clj
@@ -0,0 +1,84 @@
+(ns overtone-roland-a300.midi
+  (:require [overtone.sc.node :refer [ctl]]
+            [overtone.libs.counters :refer [next-id]]
+            [overtone.libs.event :refer [on-event
+                                         remove-handler]]
+            [overtone.music.pitch :refer [midi->hz]]))
+
+(defonce midi-key-players (atom {}))
+
+(defn bend-note [note offset]
+  (let [bent-note (+ note offset)]
+    [:note (int bent-note)
+     :freq (midi->hz bent-note)]))
+
+(defn play-midi-keys
+  [player-fn & {:keys [down-event
+                        up-event
+                        bend-event
+                        bend-extent
+                        bend?]
+                 :or {down-event [:midi :key :down]
+                      up-event [:midi :key :up]
+                      bend-event [:midi :bend]
+                      bend-extent 4
+                      bend? true}}]
+
+  (let [down-event-key (concat [:play-midi-keys] down-event)
+        up-event-key (concat [:play-midi-keys] up-event)
+        bend-event-key (concat [:play-midi-keys] bend-event)
+        notes (atom {})
+        bend-offset (atom 0)]
+
+    (on-event
+      down-event
+      (fn [{note :note
+            velocity-f :velocity-f}]
+        (let [play-args (concat (bend-note note @bend-offset)
+                                [:velocity-f velocity-f])
+              node-id (apply player-fn play-args)]
+          (swap! notes assoc note node-id)))
+      down-event-key)
+
+    (on-event
+      up-event
+      (fn [{note :note}]
+        (when-let [n (get @notes note)]
+          (ctl n :gate 0)
+          (swap! notes dissoc note)))
+      up-event-key)
+
+    (when bend?
+      (on-event
+        bend-event
+        (fn [{bending-f :bending-f}]
+          (reset! bend-offset (* bend-extent bending-f))
+
+          (doseq
+            [[note node-id] @notes] 
+            (apply ctl (concat [node-id] (bend-note @bend-offset note)))))
+        bend-event-key))
+
+     (let [player-id (next-id :play-midi-keys)
+           player {:player-id player-id
+                   :down-event-key down-event-key
+                   :up-event-key up-event-key
+                   :bend-event-key (if bend-event bend-event-key nil)
+                   :notes notes}]
+       (swap! midi-key-players assoc player-id player)
+       player-id)))
+
+(defn remove-player [player-id]
+  (let [{down-event-key :down-event-key
+         up-event-key :up-event-key
+         bend-event-key :bend-event-key} (get @midi-key-players player-id)]
+    (remove-handler down-event-key)
+    (remove-handler up-event-key)
+    (when bend-event-key
+      (remove-handler bend-event-key))
+    (swap! midi-key-players dissoc player-id)))
+
+(defn remove-all-players []
+  (doseq
+    [[player-id player] @midi-key-players] 
+    (remove-player player-id)))
---
+++ test/overtone_roland_a300/midi_test.clj
@@ -0,0 +1,199 @@
+(ns overtone-roland-a300.midi-test
+  (:require [speclj.core :refer :all]
+            [overtone.sc.node :refer [ctl]]
+            [overtone.libs.counters :refer [next-id
+                                            reset-counter!]]
+            [overtone.libs.event :refer [event
+                                         remove-handler]]
+            [overtone.music.pitch :refer [midi->hz]]
+            [overtone-roland-a300.midi :refer :all]))
+
+(def ctls (atom []))
+(def plays (atom []))
+(def removed-handlers (atom []))
+
+(defn play [& args]
+  (swap! plays conj args)
+  (next-id ::node))
+
+(describe
+  "midi"
+
+  (after
+    [(reset-counter! :play-midi-keys)])
+
+  (describe
+    "play-midi-keys"
+
+    (around [it]
+      [(reset! ctls [])
+       (reset! plays [])
+       (with-redefs [ctl #(swap! ctls conj %&)]
+         (it))
+       (remove-all-players)
+       (reset-counter! ::node)])
+
+    (describe
+      "when a key is pressed"
+
+      (it
+        "should play the player function"
+        (let [player-id (play-midi-keys play)]
+          (event
+            [:midi :key :down]
+            {:note 60
+             :velocity-f 0.5})
+
+          (event
+            [:midi :key :down]
+            {:note 61
+             :velocity-f 0.5})
+
+          (Thread/sleep 50)
+
+          (should= @plays [[:note 60
+                            :freq (midi->hz 60)
+                            :velocity-f 0.5]
+                           [:note 61
+                            :freq (midi->hz 61)
+                            :velocity-f 0.5]]))))
+
+    (describe
+      "when a key is released"
+
+      (it
+        "should zeroize the gate of the player function for that note"
+        (let [player-id (play-midi-keys play)]
+          (event
+            [:midi :key :down]
+            {:note 60
+             :velocity-f 0.5})
+          (Thread/sleep 50)
+
+          (event
+            [:midi :key :down]
+            {:note 61
+             :velocity-f 0.5})
+          (Thread/sleep 50)
+
+          (event
+            [:midi :key :up]
+            {:note 61
+             :velocity-f 0.5})
+          (Thread/sleep 50)
+
+          (should= @ctls [[1 :gate 0]]))))
+
+    (describe
+      "when the bender is bent"
+
+      (it
+        "should bend the notes currently being played"
+        (let [player-id (play-midi-keys play)]
+          (event
+            [:midi :key :down]
+            {:note 60
+             :velocity-f 0.5})
+          (Thread/sleep 50)
+
+          (event
+            [:midi :key :down]
+            {:note 61
+             :velocity-f 0.5})
+          (Thread/sleep 50)
+
+          (event
+            [:midi :bend]
+            {:bending-f 0.5})
+          (Thread/sleep 50)
+
+          (should= @ctls [[1
+                           :note 63
+                           :freq (midi->hz 63)]
+                          [0
+                           :note 62
+                           :freq (midi->hz 62)]])))
+
+      (it
+        "should bend newly played notes"
+        (let [player-id (play-midi-keys play)]
+          (event
+            [:midi :bend]
+            {:bending-f 0.5})
+          (Thread/sleep 50)
+
+          (event
+            [:midi :key :down]
+            {:note 60
+             :velocity-f 0.5})
+          (Thread/sleep 50)
+
+          (event
+            [:midi :key :down]
+            {:note 61
+             :velocity-f 0.5})
+          (Thread/sleep 50)
+
+          (should= @plays [[:note 62
+                            :freq (midi->hz 62)
+                            :velocity-f 0.5]
+                           [:note 63
+                            :freq (midi->hz 63)
+                            :velocity-f 0.5]])))))
+
+  (describe
+    "remove-player"
+
+    (around [it]
+      [(reset! removed-handlers [])
+       (with-redefs [remove-handler #(swap! removed-handlers conj %&)]
+         (it))
+       (remove-all-players)])
+
+    (it
+      "should stop listening to key press events associated with the player"
+      (remove-player (play-midi-keys play))
+      (should-contain [[:play-midi-keys :midi :key :down]] @removed-handlers))
+
+    (it
+      "should stop listening to key release events associated with the player"
+      (remove-player (play-midi-keys play))
+      (should-contain [[:play-midi-keys :midi :key :up]] @removed-handlers))
+
+    (it
+      "should stop listening to bend events associated with the player"
+      (remove-player (play-midi-keys play))
+      (should-contain [[:play-midi-keys :midi :bend]] @removed-handlers)))
+
+  (describe
+    "remove-all-players"
+
+    (around [it]
+      [(remove-all-players)
+       (reset! removed-handlers [])
+       (with-redefs [remove-handler #(swap! removed-handlers conj %&)]
+         (it))])
+
+    (it
+      "should stop listening to key press events for all players"
+      (play-midi-keys play :up-event [:foo :down])
+      (play-midi-keys play :up-event [:bar :down])
+      (remove-all-players)
+      (should-contain [[:play-midi-keys :foo :down]] @removed-handlers)
+      (should-contain [[:play-midi-keys :bar :down]] @removed-handlers))
+
+    (it
+      "should stop listening to key release events for all players"
+      (play-midi-keys play :down-event [:foo :down])
+      (play-midi-keys play :down-event [:bar :down])
+      (remove-all-players)
+      (should-contain [[:play-midi-keys :foo :down]] @removed-handlers)
+      (should-contain [[:play-midi-keys :bar :down]] @removed-handlers))
+
+    (it
+      "should stop listening to bend events for all players"
+      (play-midi-keys play :bend-event [:foo :bend])
+      (play-midi-keys play :bend-event [:bar :bend])
+      (remove-all-players)
+      (should-contain [[:play-midi-keys :foo :bend]] @removed-handlers)
+      (should-contain [[:play-midi-keys :bar :bend]] @removed-handlers))))

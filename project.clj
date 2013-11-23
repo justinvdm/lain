21e2cfbc305bd1362c6a1b40ba8320c4fbdb7b53
justinvdm <justinvdm.0.1.0@gmail.com>
2013-11-23 11:53:59 +0200

    Downgrade speclj and clojure, sadly this seems necessary in order for overtone and speclj to play nice

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
                midi keyboard in overtone"
   :url "https://github.com/justinvdm/overtone-roland-a300"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone "0.8.1"]]
-  :profiles {:dev {:dependencies [[speclj "2.8.1"]]}}
-  :plugins [[speclj "2.8.1"]]
+  :profiles {:dev {:dependencies [[speclj "2.7.5"]]}}
+  :plugins [[speclj "2.7.5"]]
   :test-paths ["test"])

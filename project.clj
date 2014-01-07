82775c148ecf830025a22f8cfe75395e1c3f1b1a
justinvdm <justinvdm.0.1.0@gmail.com>
2014-01-08 00:37:57 +0200

    Update overtone version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/justinvdm/lain"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.8.1" :exclusions [org.clojure/clojure]]
+                 [overtone "0.9.1" :exclusions [org.clojure/clojure]]
                  [a300 "0.0.0"]]
   :profiles {:dev {:dependencies [[speclj "2.7.5"]]}}
   :plugins [[speclj "2.7.5"]]

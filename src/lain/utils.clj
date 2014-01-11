3770d47776f5924b7133288c7e099000a6978892
justinvdm <justinvdm.0.1.0@gmail.com>
2014-01-11 13:37:25 +0200

    Clean up lain.utils a bit

--- src/lain/utils.clj
+++ src/lain/utils.clj
@@ -7,11 +7,12 @@
 (defmacro temp-inst
   "Workaround to declare an inst with params without using definst
   https://github.com/overtone/overtone/issues/248"
-  [i-name & inst-form]
+  [inst-name & inst-form]
   {:arglists '([name doc-string? params ugen-form])}
-  (let [[i-name params ugen-form] (synth-form i-name inst-form)
-         i-name                    (with-meta i-name (merge (meta i-name) {:type ::instrument}))]
-    `(inst ~i-name ~params ~ugen-form)))
+  (let
+    [[inst-name params ugen-form] (synth-form inst-name inst-form)
+      inst-name (with-meta inst-name (merge (meta inst-name) {:type ::instrument}))]
+    `(inst ~inst-name ~params ~ugen-form)))
 
 (defn key-inst
   [wave
@@ -21,9 +22,10 @@
     [freq 440
      velocity-f 1
      gate 1]
-    (let [wave-val (wave :freq freq)
-          env-val (env-gen env gate :action FREE)]
-    (* velocity-f env-val wave-val))))
+    (let
+      [wave-val (wave :freq freq)
+       env-val (env-gen env gate :action FREE)]
+      (* velocity-f env-val wave-val))))
 
 (defn key-note-inst
   [wave
@@ -33,6 +35,7 @@
     [note 60
      velocity-f 1
      gate 1]
-    (let [wave-val (wave :note note)
-          env-val (env-gen env gate :action FREE)]
-    (* velocity-f env-val wave-val))))
+    (let
+      [wave-val (wave :note note)
+       env-val (env-gen env gate :action FREE)]
+      (* velocity-f env-val wave-val))))

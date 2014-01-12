2e96665644ef820a352028a0f615e54e05823dbf
justinvdm <justinvdm.0.1.0@gmail.com>
2014-01-12 12:20:18 +0200

    Wrap up warped experiment

--- src/lain/experiments/warped.clj
+++ src/lain/experiments/warped.clj
@@ -3,26 +3,28 @@
             [a300.events :refer [handle-a300-events]]
             [a300.play :refer [midi-key-player
                                remove-key-player]]
-            [lain.utils :refer [key-inst]]))
+            [lain.utils :refer [key-inst
+                                deflcgen]]))
 
-(defsynth note-warp
-  [freq 2
-   extent 1]
-  (out
-    -b-warp
+(def !warp-bus (audio-bus))
+
+(definst warp
+  [freq 0.5 
+   extent 4]
+  (out:ar
+    !warp-bus
     (range-lin (sin-osc freq) (- extent) extent)))
+
+(deflcgen wave :ar
+  [freq 440]
+  (let
+    [offset (in:ar !warp-bus)
+     freq (+ freq offset)]
+    (square freq)))
 
 (comment
   (handle-a300-events)
-
-  (def -b-warp (audio-bus))
-  (def -b-waves-num (control-bus))
-  (def -b-waves-diff (control-bus))
-
-  (control-bus-set! -b-waves-num 3)
-  (control-bus-set! -b-waves-diff 1)
-
-  (def -s-note-warp (-s-note-warp))
-  (def -i-ki (key-inst saw (adsr 0.0001 0.01 1 2.5)))
+  (def !warp (warp))
+  (def -i-ki (key-inst wave (adsr 0.0001 0.01 1 2.5)))
   (def -k (midi-key-player -i-ki))
 ())

11b2d151e698ee6e478530dbc5928306d15f36a5
justinvdm <justinvdm.0.1.0@gmail.com>
2014-01-19 21:04:40 +0200

    Use the a300's new `value-controller` in the pitched sampler experiment (and do some cleanup)

--- src/lain/experiments/pitched_sample.clj
+++ src/lain/experiments/pitched_sample.clj
@@ -2,15 +2,21 @@
   (:require [overtone.live :refer :all]
             [a300.events :refer [handle-a300-events]]
             [a300.play :refer [midi-key-player
-                               remove-key-player]]
+                               remove-all-key-players]]
+            [a300.control :refer [value-controller
+                                  remove-all-controllers]]
             [lain.utils :refer [key-inst
                                 deflcgen
                                 get-sample]]))
 
+(def !window-size-bus (control-bus))
+(def !window-rand-ratio-bus (control-bus))
+(def !overlaps-bus (control-bus))
+
 (def !glock-buf (get-sample "glock1/c1.wav"))
 (def !base-freq (midi->hz 60))  ; c1
 
-(deflcgen glock :ar
+(deflcgen glock-wave :ar
   [freq 440]
   (let
     [scale (/ freq !base-freq)]
@@ -18,20 +24,29 @@
       :num-channels 1
       :bufnum !glock-buf
       :freq-scale scale
-      :window-size 0.1
-      :overlaps 8
-      :window-rand-ratio 0.5
+      :window-size (in:kr !window-size-bus)
+      :overlaps (in:kr !overlaps-bus)
+      :window-rand-ratio (in:kr !window-rand-ratio-bus)
       :interp 4)))
+
+(def glock
+  (key-inst
+    glock-wave
+    (adsr :attack 0.0001
+          :decay 0.01
+          :sustain 1
+          :release 2.5 :level 2)))
+
 
 (comment
   (handle-a300-events)
-  (def -i-ki (key-inst
-               glock
-               (adsr
-                 :attack 0.0001
-                 :decay 0.01
-                 :sustain 1
-                 :release 2.5
-                 :level 2)))
-  (def -k (midi-key-player -i-ki))
+
+  (value-controller !window-size-bus [:midi :r1])
+  (value-controller !window-rand-ratio-bus [:midi :r2])
+  (value-controller !overlaps-bus [:midi :r3] :extent [0 8])
+
+  (midi-key-player glock)
+
+  (remove-all-controllers)
+  (remove-all-key-players)
 ())

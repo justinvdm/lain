20ee5d1930ef5f0e541d78f3056e0b150e960712
justinvdm <justinvdm.0.1.0@gmail.com>
2014-03-22 11:48:20 +0200

    Refactor midi-player out of midi-key-player and buf-player

--- src/lain/a300/play.clj
+++ src/lain/a300/play.clj
@@ -6,13 +6,82 @@
             [overtone.music.pitch :refer [midi->hz]]
             [overtone.sc.envelope :refer [perc]]))
 
-(defonce players (atom {}))
+(defonce midi-players (atom {}))
 
 (defn bend-note [note offset]
   (let [bent-note (+ note offset)]
     [:note (int bent-note)
      :freq (midi->hz bent-note)]))
 
+
+(defn midi-player [player-key & {:keys [down-event 
+                                        up-event
+                                        down
+                                        up
+                                        setup
+                                        teardown
+                                        device-name]
+                                 :or {down-event [:midi :key :down]
+                                      up-event [:midi :key :up]
+                                      down (fn [e] ())
+                                      up (fn [e v] ())
+                                      setup identity
+                                      teardown (fn [p] ())
+                                      device-name nil}}]
+  (let [down-event-key (concat [player-key] down-event)
+        up-event-key (concat [player-key] up-event)
+        notes (atom {})]
+
+    (on-event
+      down-event
+      (fn [e]
+        (let [{note :note
+               {event-device-name :name} :device} e]
+          (when (and (or (nil? device-name)
+                         (= device-name event-device-name))
+                     (not (contains? @notes note)))
+            (swap! notes assoc note (down e)))))
+      down-event-key)
+
+    (on-event
+      up-event
+      (fn [e]
+        (let [{note :note
+              {event-device-name :name} :device} e]
+          (when (and (or (nil? device-name)
+                         (= device-name event-device-name))
+                     (contains? @notes note))
+              (up e (get @notes note))
+              (swap! notes dissoc note))))
+      up-event-key)
+
+    (let [player-id (next-id :midi-player)
+          player {:player-id player-id
+                  :setup setup
+                  :teardown teardown
+                  :down-event-key down-event-key
+                  :up-event-key up-event-key
+                  :notes notes}
+          player (setup player)]
+      (swap! midi-players assoc player-id player)
+      player-id)))
+
+
+(defn remove-midi-player [player-id]
+  (let [player (get @midi-players player-id)
+        {down-event-key :down-event-key
+         up-event-key :up-event-key} player]
+    ((:teardown player) player)
+    (remove-event-handler down-event-key)
+    (remove-event-handler up-event-key)
+    (swap! midi-players dissoc player-id)))
+
+
+(defn remove-all-midi-players []
+  (doseq
+    [[player-id player] @midi-players] 
+    (remove-midi-player player-id)))
+  
 
 (defn midi-key-player [player-fn & {:keys [down-event
                                            up-event
@@ -21,115 +90,56 @@
                                          up-event [:midi :key :up]
                                          device-name nil}}]
 
-  (let [down-event-key (concat [:midi-key-player] down-event)
-        up-event-key (concat [:midi-key-player] up-event)
-        notes (atom {})
-        bend-offset (atom 0)]
+  (let [bend-offset (atom 0)]
+    (midi-player
+      :key-player
+      :device-name device-name
+      :down-event down-event
+      :up-event up-event
 
-    (on-event
-      down-event
+      :setup
+      (fn [player]
+        (assoc player :bend-offset bend-offset))
+
+      :down
       (fn [{note :note
-            velocity-f :velocity-f
-            {event-device-name :name} :device}]
-        (when (or (nil? device-name)
-                  (= device-name event-device-name))
-          (when-not (contains? @notes note)
-            (let [play-args (concat (bend-note note @bend-offset)
-                                    [:velocity-f velocity-f])
-                  node-id (apply player-fn play-args)]
-              (swap! notes assoc note node-id)))))
-      down-event-key)
+            velocity-f :velocity-f}]
+        (let [play-args (concat (bend-note note @bend-offset)
+                                [:velocity-f velocity-f])]
+          (apply player-fn play-args)))
 
-    (on-event
-      up-event
-      (fn [{note :note
-            {event-device-name :name} :device}]
-        (when (or (nil? device-name)
-                  (= device-name event-device-name))
-          (when-let [n (get @notes note)]
-            (ctl n :gate 0)
-            (swap! notes dissoc note))))
-      up-event-key)
-
-    (let [player-id (next-id :midi-key-player)
-          player {:player-id player-id
-                  :down-event-key down-event-key
-                  :up-event-key up-event-key
-                  :notes notes
-                  :bend-offset bend-offset}]
-      (swap! players assoc player-id player)
-      player-id)))
+      :up
+      (fn [e node-id]
+        (ctl node-id :gate 0)))))
 
 
 (defn bend-midi-keys
   [player-id
    bend-offset]
-  (let [player (get @players player-id)]
+  (let [player (get @midi-players player-id)]
     (reset! (:bend-offset player) bend-offset)
     (doseq
       [[note node-id] @(:notes player)] 
       (apply ctl (concat [node-id] (bend-note bend-offset note))))))
 
 
-(defn remove-key-player [player-id]
-  (let [{down-event-key :down-event-key
-         up-event-key :up-event-key} (get @players player-id)]
-    (remove-event-handler down-event-key)
-    (remove-event-handler up-event-key)
-    (swap! players dissoc player-id)))
-
-
-(defn remove-all-key-players []
-  (doseq
-    [[player-id player] @players] 
-    (remove-key-player player-id)))
-
-
 (defn buf-player [player-fn bufs & {:keys [down-event
-                                           up-event]
+                                           up-event
+                                           device-name]
                                     :or {down-event [:midi :pad :down]
-                                         up-event [:midi :pad :up]}}]
+                                         up-event [:midi :pad :up]
+                                         device-name nil}}]
 
-  (let [down-event-key (concat [:buf-player] down-event)
-        up-event-key (concat [:buf-player] up-event)
-        notes (atom {})]
+  (midi-player
+    :buf-player
+    :device-name device-name
+    :down-event down-event
+    :up-event up-event
 
-    (on-event
-      down-event
-      (fn [{note :note
-            velocity-f :velocity-f}]
-        (when-not (contains? @notes note)
-          (when-let [buf (get bufs note)]
-            (swap! notes assoc note velocity-f)
-            (player-fn
-              :buf buf
-              :velocity-f velocity-f))))
-      down-event-key)
-
-    (on-event
-      up-event
-      (fn [{note :note}]
-        (swap! notes dissoc note))
-      up-event-key)
-
-    (let [player-id (next-id :buf-player)
-          player {:player-id player-id
-                  :down-event-key down-event-key
-                  :up-event-key up-event-key
-                  :notes notes}]
-      (swap! players assoc player-id player)
-      player-id)))
-
-
-(defn remove-buf-player [player-id]
-  (let [{down-event-key :down-event-key
-         up-event-key :up-event-key} (get @players player-id)]
-    (remove-event-handler down-event-key)
-    (remove-event-handler up-event-key)
-    (swap! players dissoc player-id)))
-
-
-(defn remove-all-buf-players []
-  (doseq
-    [[player-id player] @players] 
-    (remove-key-player player-id)))
+    :down
+    (fn [{note :note
+          velocity-f :velocity-f}]
+      (when-let [buf (get bufs note)]
+        (player-fn
+          :buf buf
+          :velocity-f velocity-f)))))
--- test/a300/play_test.clj
+++ test/a300/play_test.clj
@@ -2,37 +2,27 @@
   (:require [speclj.core :refer :all]
             [overtone.sc.node :refer [ctl]]
             [overtone.libs.counters :refer [next-id
-                                            reset-counter!]]
+                                            reset-all-counters!]]
             [overtone.libs.event :refer [sync-event
                                          remove-event-handler]]
             [overtone.music.pitch :refer [midi->hz]]
             [lain.a300.play :refer :all]))
 
-(def ctls (atom []))
-(def plays (atom []))
-(def removed-handlers (atom []))
-
-(defn play [& args]
-  (swap! plays conj args)
-  (next-id ::node))
-
 (describe "play"
+  (with-stubs)
 
-  (around [it]
-    [(reset! ctls [])
-     (reset! plays [])
-     (with-redefs [ctl #(swap! ctls conj %&)]
-       (it))
-     (remove-all-key-players)
-     (remove-all-buf-players)
-     (reset-counter! ::node)
-     (reset-counter! :play-midi-keys)])
+  (before
+    [(remove-all-midi-players)
+     (reset-all-counters!)])
 
-  (describe "midi-key-player"
+  (describe "midi-player"
+    (it "should invoke its setup hook"
+      (midi-player :test-player :setup (stub :setup {:invoke identity}))
+      (should-have-invoked :setup {:times 1}))
 
     (describe "when a key is pressed"
-      (it "should play the player function if the key is not already pressed"
-        (midi-key-player play)
+      (it "should handle the event"
+        (midi-player :test-player :down (stub :down))
 
         (sync-event
           [:midi :key :down]
@@ -44,30 +34,33 @@
           {:note 61
            :velocity-f 0.5})
 
-        (should= @plays [[:note 60
-                          :freq (midi->hz 60)
-                          :velocity-f 0.5]
-                         [:note 61
-                          :freq (midi->hz 61)
-                          :velocity-f 0.5]]))
+        (should-have-invoked :down {:with [{:note 60
+                                            :velocity-f 0.5}]
+                                    :times 1})
 
-      (it "should not play the player function if the key is already pressed"
-        (midi-key-player play)
+        (should-have-invoked :down {:with [{:note 61
+                                            :velocity-f 0.5}]
+                                    :times 1}))
 
-        (sync-event
-          [:midi :key :down]
-          {:note 60
-           :velocity-f 0.5})
+      (it "should not handle the event if the key is already pressed"
+        (midi-player :test-player :down (stub :down))
 
         (sync-event
           [:midi :key :down]
           {:note 60
            :velocity-f 0.5})
 
-        (should= 1 (count @plays)))
+        (sync-event
+          [:midi :key :down]
+          {:note 60
+           :velocity-f 0.5})
 
-      (it "should not play the player function if the key belongs to another device"
-        (midi-key-player play :device "device-foo")
+        (should-have-invoked :down {:times 1}))
+
+      (it "should not handle the event if the key belongs to another device"
+        (midi-player :test-player
+                     :device "device-foo"
+                     :down (stub :down))
 
         (sync-event
           [:midi :key :down]
@@ -81,12 +74,14 @@
            :velocity-f 0.5
            :device {:name "device-bar"}})
 
-        (should= 1 (count @plays)))))
+        (should-have-invoked :down {:times 1})))
 
     (describe "when a key is released"
-
-      (it "should zeroize the gate of the player function for that note"
-        (midi-key-player play)
+      (it "should handle the event"
+        (midi-player
+          :test-player
+          :down (stub :down {:return true})
+          :up (stub :up))
 
         (sync-event
           [:midi :key :down]
@@ -103,10 +98,36 @@
           {:note 61
            :velocity-f 0.5})
 
-        (should= @ctls [[1 :gate 0]]))
+        (should-have-invoked :up {:with [{:note 61
+                                          :velocity-f 0.5}
+                                         true]
+                                  :times 1})))
 
-      (it "should ignore the key if it belongs to another device"
-        (midi-key-player play :device "device-foo")
+      (it "should ignore the event if no corresponding down event has happened"
+        (midi-player :test-player :up (stub :up))
+
+        (sync-event
+          [:midi :key :down]
+          {:note 60
+           :velocity-f 0.5})
+
+        (sync-event
+          [:midi :key :down]
+          {:note 60
+           :velocity-f 0.5})
+
+        (sync-event
+          [:midi :key :up]
+          {:note 60
+           :velocity-f 0.5})
+
+        (should-have-invoked :up {:times 1}))
+
+      (it "should ignore the event if it belongs to another device"
+        (midi-player
+          :test-player
+          :device "device-foo"
+          :up (stub :up))
 
         (sync-event
           [:midi :key :down]
@@ -126,99 +147,140 @@
            :velocity-f 0.5
            :device {:name "device-bar"}})
 
-        (should= 1 (count @ctls))))
+        (should-have-invoked :up {:times 1})))
 
-  (describe "bend-midi-keys"
-
-    (it
-      "should bend the key player's active notes"
-      (let [player-id (midi-key-player play)]
-        (sync-event
-          [:midi :key :down]
-          {:note 60
-           :velocity-f 0.5})
-
-        (sync-event
-          [:midi :key :down]
-          {:note 61
-           :velocity-f 0.5})
-
-        (bend-midi-keys player-id 2)
-
-        (should= @ctls [[1
-                         :note 63
-                         :freq (midi->hz 63)]
-                        [0
-                         :note 62
-                         :freq (midi->hz 62)]])))
-
-    (it "should bend new notes played by the key player"
-      (let [player-id (midi-key-player play)]
-        (bend-midi-keys player-id 2)
-
-        (sync-event
-          [:midi :key :down]
-          {:note 60
-           :velocity-f 0.5})
-
-        (sync-event
-          [:midi :key :down]
-          {:note 61
-           :velocity-f 0.5})
-
-        (should= @plays [[:note 62
-                          :freq 293.6647679174076
-                          :velocity-f 0.5]
-                         [:note 63
-                          :freq 311.1269837220809
-                          :velocity-f 0.5]]))))
-
-  (describe "remove-key-player"
-
-    (around [it]
-      [(reset! removed-handlers [])
-       (with-redefs [remove-event-handler #(swap! removed-handlers conj %&)]
-         (it))
-       (remove-all-key-players)])
+  (describe "remove-midi-player"
+    (it "should invoke the player's teardown hook"
+      (remove-midi-player (midi-player :test-player :teardown (stub :teardown)))
+      (should-have-invoked :teardown {:times 1}))
 
     (it "should stop listening to key press events associated with the player"
-      (remove-key-player (midi-key-player play))
-      (should-contain [[:midi-key-player :midi :key :down]] @removed-handlers))
+      (should-invoke
+        remove-event-handler
+        {:with [[:test-player :midi :key :down]]}
+        (remove-midi-player (midi-player :test-player))))
 
     (it "should stop listening to key release events associated with the player"
-      (remove-key-player (midi-key-player play))
-      (should-contain [[:midi-key-player :midi :key :up]] @removed-handlers)))
+      (should-invoke
+        remove-event-handler
+        {:with [[:test-player :midi :key :up]]}
+        (remove-midi-player (midi-player :test-player)))))
 
-  (describe "remove-all-key-players"
+  (describe "remove-all-midi-players"
+    (it "should remove all midi players"
+      (midi-player :foo)
+      (midi-player :bar)
 
-    (around [it]
-      [(remove-all-key-players)
-       (reset! removed-handlers [])
-       (with-redefs [remove-event-handler #(swap! removed-handlers conj %&)]
-         (it))])
+      (should-invoke
+        remove-midi-player
+        {:with [0] :times 1}
+        {:with [1] :times 1}
+        (remove-all-midi-players))))
 
-    (it "should stop listening to key press events for all players"
-      (midi-key-player play :up-event [:foo :down])
-      (midi-key-player play :up-event [:bar :down])
-      (remove-all-key-players)
-      (should-contain [[:midi-key-player :foo :down]] @removed-handlers)
-      (should-contain [[:midi-key-player :bar :down]] @removed-handlers))
+  (describe "midi-key-player"
+    (describe "when a key is pressed"
+      (it "should play the player function"
+        (midi-key-player (stub :play))
 
-    (it "should stop listening to key release events for all players"
-      (midi-key-player play :down-event [:foo :down])
-      (midi-key-player play :down-event [:bar :down])
-      (remove-all-key-players)
-      (should-contain [[:midi-key-player :foo :down]] @removed-handlers)
-      (should-contain [[:midi-key-player :bar :down]] @removed-handlers)))
+        (sync-event
+          [:midi :key :down]
+          {:note 60
+           :velocity-f 0.5})
 
+        (sync-event
+          [:midi :key :down]
+          {:note 61
+           :velocity-f 0.5})
+
+        (should-have-invoked :play {:with [:note 60
+                                           :freq (midi->hz 60)
+                                           :velocity-f 0.5]
+                                    :times 1})
+
+        (should-have-invoked :play {:with [:note 61
+                                           :freq (midi->hz 61)
+                                           :velocity-f 0.5]
+                                    :times 1})))
+
+    (describe "when a key is released"
+      (it "should zeroize the gate of the player function for that note"
+        (midi-key-player
+          (stub :play {:invoke (fn [_ _ _ _ _ _] (next-id :fake-node))}))
+
+        (should-invoke
+          ctl
+          {:with [1 :gate 0]
+           :times 1}
+          (sync-event
+            [:midi :key :down]
+            {:note 60
+             :velocity-f 0.5})
+
+          (sync-event
+            [:midi :key :down]
+            {:note 61
+             :velocity-f 0.5})
+
+          (sync-event
+            [:midi :key :up]
+            {:note 61
+             :velocity-f 0.5}))))
+
+  (describe "bend-midi-keys"
+    (it "should bend the key player's active notes"
+      (let [play (stub :play {:invoke (fn [_ _ _ _ _ _] (next-id :fake-node))})
+            player-id (midi-key-player play)]
+
+        (sync-event
+            [:midi :key :down]
+            {:note 60
+             :velocity-f 0.5})
+          (sync-event
+            [:midi :key :down]
+            {:note 61
+             :velocity-f 0.5})
+
+        (should-invoke
+          ctl
+          {:with [0
+                  :note 62
+                  :freq (midi->hz 62)]
+           :times 1}
+          {:with [1
+                  :note 63
+                  :freq (midi->hz 63)]
+           :times 1}
+          (bend-midi-keys player-id 2))))
+
+    (it "should bend new notes played by the key player"
+      (let [player-id (midi-key-player (stub :play))]
+        (bend-midi-keys player-id 2)
+
+        (sync-event
+          [:midi :key :down]
+          {:note 60
+           :velocity-f 0.5})
+
+        (sync-event
+          [:midi :key :down]
+          {:note 61
+           :velocity-f 0.5})
+
+
+        (should-have-invoked :play {:with [:note 62
+                                           :freq 293.6647679174076
+                                           :velocity-f 0.5]
+                                    :times 1})
+        (should-have-invoked :play {:with [:note 63
+                                           :freq 311.1269837220809
+                                           :velocity-f 0.5]
+                                    :times 1}))))
 
   (describe "buf-player"
-
     (describe "when the down event is emitted"
-
-      (it "should only play the function once until the up event is emitted"
-        (buf-player play {2 "buf-2"
-                          3 "buf-3"})
+      (it "should play the player function"
+        (buf-player (stub :play) {2 "buf-2"
+                                  3 "buf-3"})
 
         (sync-event
           [:midi :pad :down]
@@ -230,77 +292,9 @@
           {:note 3
            :velocity-f 0.5})
 
-        (sync-event
-          [:midi :pad :down]
-          {:note 2
-           :velocity-f 0.3})
-
-        (sync-event
-          [:midi :pad :down]
-          {:note 3
-           :velocity-f 0.2})
-
-        (sync-event
-          [:midi :pad :up]
-          {:note 2})
-
-        (sync-event
-          [:midi :pad :up]
-          {:note 3})
-
-        (sync-event
-          [:midi :pad :down]
-          {:note 2
-           :velocity-f 0.8})
-
-        (sync-event
-          [:midi :pad :down]
-          {:note 3
-           :velocity-f 0.9})
-
-        (should= @plays [[:buf "buf-2"
-                          :velocity-f 0.6]
-                         [:buf "buf-3"
-                          :velocity-f 0.5]
-                         [:buf "buf-2"
-                          :velocity-f 0.8]
-                         [:buf "buf-3"
-                          :velocity-f 0.9]]))))
-
-  (describe "remove-buf-player"
-
-    (around [it]
-      [(reset! removed-handlers [])
-       (with-redefs [remove-event-handler #(swap! removed-handlers conj %&)]
-         (it))
-       (remove-all-buf-players)])
-
-    (it "should stop listening to down events associated with the player"
-      (remove-buf-player (buf-player play {}))
-      (should-contain [[:buf-player :midi :pad :down]] @removed-handlers))
-
-    (it "should stop listening to up events associated with the player"
-      (remove-buf-player (buf-player play {}))
-      (should-contain [[:buf-player :midi :pad :up]] @removed-handlers))
-
-  (describe "remove-all-buf-players"
-
-    (around [it]
-      [(remove-all-buf-players)
-       (reset! removed-handlers [])
-       (with-redefs [remove-event-handler #(swap! removed-handlers conj %&)]
-         (it))])
-
-    (it "should stop listening to down events for all players"
-      (buf-player play {} :down-event [:foo :down])
-      (buf-player play {} :down-event [:bar :down])
-      (remove-all-buf-players)
-      (should-contain [[:buf-player :foo :down]] @removed-handlers)
-      (should-contain [[:buf-player :bar :down]] @removed-handlers))
-
-    (it "should stop listening to up events for all players"
-      (buf-player play {} :up-event [:foo :up])
-      (buf-player play {} :up-event [:bar :up])
-      (remove-all-buf-players)
-      (should-contain [[:buf-player :foo :up]] @removed-handlers)
-      (should-contain [[:buf-player :bar :up]] @removed-handlers)))))
+        (should-have-invoked :play {:with [:buf "buf-2"
+                                           :velocity-f 0.6]
+                                    :times 1})
+        (should-have-invoked :play {:with [:buf "buf-3"
+                                           :velocity-f 0.5]
+                                    :times 1}))))))

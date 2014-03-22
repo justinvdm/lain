a99e7967f1d4a8457abe169446e07fb13d074398
justinvdm <justinvdm.0.1.0@gmail.com>
2014-03-22 11:47:06 +0200

    Update speclj dependency to 3.0.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [overtone "0.10-SNAPSHOT" :exclusions [org.clojure/clojure]]
                  [a300 "0.0.0"]]
-  :profiles {:dev {:dependencies [[speclj "2.7.5"]]}}
+  :profiles {:dev {:dependencies [[speclj "3.0.0"]]}}
   :plugins [[speclj "2.7.5"]]
   :test-paths ["test"]
   :speclj-eval-in :leiningen)

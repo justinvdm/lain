8fe48c3e94cd9199c3e7d678bad5ab35e563fe75
justinvdm <justinvdm.0.1.0@gmail.com>
2014-05-02 20:31:49 +0200

    tests for metronome

--- test/lain/utils_test.clj
+++ test/lain/utils_test.clj
@@ -1,10 +1,26 @@
 (ns lain.utils-test
   (:require [speclj.core :refer :all]
+            [mecha.core :as mecha]
+            [overtone.sc.node :refer [kill]]
+            [overtone.sc.synth :refer [synth]]
             [overtone.sc.sample :refer [load-samples]]
+            [overtone.sc.ugens :refer [in:kr
+                                       replace-out:kr
+                                       pulse-count:kr]]
+            [overtone.sc.bus :refer [free-bus
+                                     control-bus
+                                     control-bus-get
+                                     control-bus-set!]]
             [lain.utils :refer :all]))
+
+
+(def c (control-bus))
+
 
 (describe "utils"
   (with-stubs)
+
+  (after (control-bus-set! c 0))
 
   (describe "lin-interpolator"
       (it "should make a linear interpolator"
@@ -25,7 +41,7 @@
            36 {:name "c2.wav"}}))
 
         (should-have-invoked :load-samples {:times 1
-                                            :with ["c1.wav" "c2.wav"]})))
+                                            :with ["c1.wav" "c2.wav"]}))
 
     (it "should support files named by midi notes"
       (with-redefs [load-samples
@@ -34,3 +50,64 @@
         (should= (load-note-samples "c1.wav" "c2.wav")
           {60 {:name "60.wav"}
            61 {:name "61.wav"}}))))
+
+  (describe "metro"
+    (it "should send beat pulses"
+      (let [m (metro :bpm (* 120 10)
+                     :bpb 4
+                     :res 4)
+            n ((synth (replace-out:kr c (pulse-count:kr (in:kr (:beats m))))))]
+
+        (should= (control-bus-get c) [0.0])
+
+        (Thread/sleep 50)
+        (should= (control-bus-get c) [1.0])
+
+        (Thread/sleep 50)
+        (should= (control-bus-get c) [2.0])
+
+        (Thread/sleep 50)
+        (should= (control-bus-get c) [3.0])
+
+        (Thread/sleep 50)
+        (should= (control-bus-get c) [4.0])
+
+        (Thread/sleep 50)
+        (should= (control-bus-get c) [5.0])
+
+        (mecha/stop m)
+        (kill n)))
+
+    (it "should send bar pulses"
+      (let [m (metro :bpm (* 120 20)
+                     :bpb 4
+                     :res 4)
+            n ((synth (replace-out:kr c (pulse-count:kr (in:kr (:bars m))))))]
+
+        (should= (control-bus-get c) [0.0])
+
+        (Thread/sleep 100)
+        (should= (control-bus-get c) [1.0])
+
+        (Thread/sleep 100)
+        (should= (control-bus-get c) [2.0])
+
+        (Thread/sleep 100)
+        (should= (control-bus-get c) [3.0])
+
+        (Thread/sleep 100)
+        (should= (control-bus-get c) [4.0])
+
+        (Thread/sleep 100)
+        (should= (control-bus-get c) [5.0])
+
+        (mecha/stop m)))
+
+    (describe "when the metronome is stopped"
+      (it "should kill its synth node"
+        (let [m (metro)]
+          (should-invoke
+            kill
+            {:with [(:node m)]
+             :times 1}
+            (mecha/stop m)))))))

b33fc4c382a7c352414c8e9a932522d9be25956a
justinvdm <justinvdm.0.1.0@gmail.com>
2014-05-04 15:26:33 +0200

    change sequencer tests to use proper synths instead of anonymous synths

--- test/lain/sequencers_test.clj
+++ test/lain/sequencers_test.clj
@@ -1,32 +1,47 @@
 (ns lain.sequencers-test
   (:require [speclj.core :refer :all]
             [mecha.core :refer [stop]]
-            [overtone.sc.synth :refer [synth]]
+            [overtone.sc.synth :refer [defsynth]]
             [overtone.sc.ugens :refer [in:kr
                                        replace-out:kr
                                        pulse-count:kr]]
-            [overtone.sc.bus :refer [control-bus]]
+            [overtone.sc.bus :refer [control-bus
+                                     control-bus-set!]]
             [lain.utils :refer [deflcgen]]
             [lain.sequencers :refer :all]))
 
 
 (deflcgen hit :kr [target 0
-                   trig 1]
+                   trig 0]
   (replace-out:kr target (pulse-count:kr (in:kr trig))))
 
 
-(defsq sq-foo 4 {(synth (hit 0)) [0 1 1 0]
-                 (synth (hit 1)) [1 0 0 1]})
+(def bus-a (control-bus))
+(defsynth syn-a [trig 0] (hit bus-a))
+
+
+(def bus-b (control-bus))
+(defsynth syn-b [trig 0] (hit bus-b))
+
+
+(def bus-c (control-bus))
+(defsynth syn-c [trig 0] (hit bus-c))
+
+
+(defsq sq-foo 4 {syn-a [0 1 1 0]
+                 syn-b [1 0 0 1]})
 
 
 (describe "sequencers"
   (with-stubs)
+
+  (after (do (control-bus-set! bus-a 0)
+             (control-bus-set! bus-b 0)
+             (control-bus-set! bus-c 0)))
   
   (describe "sq"
     (it "should normalize the sq"
-      (let [syn-a (synth (hit))
-            syn-b (synth (hit))
-            s (sq 4 {syn-a [0 1 1 0]
+      (let [s (sq 4 {syn-a [0 1 1 0]
                      syn-b [1 0 0 1]})]
         (with-redefs [normal-res 8]
           (should= {syn-a [0 0 1 0 1 0 0 0]
@@ -34,10 +49,7 @@
 
   (describe "sq+"
     (it "should construct a new sq that adds the given sq"
-      (let [syn-a (synth (hit))
-            syn-b (synth (hit))
-            syn-c (synth (hit))
-            s1 (sq 4 {syn-a [0 1 0 1]
+      (let [s1 (sq 4 {syn-a [0 1 0 1]
                       syn-b [1 0 0 1]})
             s2 (sq 4 {syn-a [0 1 1 0]
                       syn-c [1 0 1 1]})
@@ -54,10 +66,8 @@
       (should= (type sq-foo) :lain.sequencers/sq)))
 
   (describe "sequencer"
-    (it "should allow a metronome to be optional")
-    (it "should a vector of sq to be given")
-    (it "should an sq to be given")
     (it "should send beat triggers for each track")
+    (it "should allow a vector of sq to be given")
 
     (describe "when stopped"
       (it "should free its index bus")

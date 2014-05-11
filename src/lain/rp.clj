4d756eb089383e6a8d5c45b76e359f50225a9b7b
justinvdm <justinvdm.0.1.0@gmail.com>
2014-05-11 22:29:30 +0200

    a working rp!

---
+++ src/lain/rp.clj
@@ -0,0 +1,133 @@
+(ns lain.rp
+  (:require [overtone.libs.counters :refer :all]
+            [overtone.sc.node :refer :all]
+            [overtone.sc.synth :refer :all]
+            [overtone.sc.envelope :refer :all]
+            [overtone.sc.ugens :refer :all]
+            [overtone.libs.event :refer :all]
+            [overtone.sc.cgens.buf-io :refer :all]
+            [overtone.sc.bus :refer :all]
+            [overtone.sc.buffer :refer :all]
+            [lain.utils :refer [deflcgen]]
+            [lain.mecha :as mecha :refer [mecha
+                                          switch
+                                          defmecha]]))
+
+
+(def default-tick-size 1000)
+(def default-buffer-size (Math/pow 2 22))
+
+
+(deflcgen syn-if :kr [v 1
+                      then 1
+                      else 0]
+  (let [t (= v 1)
+        f (= v 0)
+        t (* t then)
+        f (* f else)
+        r (+ t f)]
+    r))
+
+
+(deflcgen syn-tick :kr [freq default-tick-size
+                        gate 1]
+  (let [p (and gate (impulse:kr freq))
+        c (pulse-count:kr p)]
+    (/ c freq)))
+
+
+(defsynth syn-rec [in-bus 0
+                   out-buf 0
+                   timer-bus 1
+                   sync-bus -1]
+  (let [sync-trig (syn-if (> sync-bus 0) (in:kr sync-bus) 1)
+        sync-trig (set-reset-ff:kr sync-trig)]
+    (out:kr timer-bus (syn-tick :gate sync-trig))
+    (record-buf:ar (in:ar in-bus) out-buf :run sync-trig :loop 0)))
+
+
+(defmecha rp-rec [& [in-bus 0
+                     sync-bus -1
+                     buf-size default-buffer-size]]
+  (:start [!out-buf (buffer buf-size)
+           !timer-bus (control-bus)
+           rec-node (syn-rec in-bus
+                             !out-buf
+                             !timer-bus
+                             sync-bus)]
+          {:node rec-node
+           :out-buf !out-buf
+           :timer-bus !timer-bus}))
+
+
+(defsynth syn-play [in-buf 0
+                    out-bus 0
+                    timer-bus 1
+                    sync-bus -1
+                    looped 1
+                    attack 0.01
+                    sustain 1
+                    release 1]
+  (let [sync-trig (set-reset-ff:kr (in:kr sync-bus) (local-in:kr))
+        sync-trig (syn-if (> sync-bus 0) sync-trig 1)
+
+        rec-len (in:kr timer-bus)
+        timer-pos (syn-tick :gate sync-trig)
+        play-pos (mod timer-pos rec-len)
+        play-done (= (round play-pos 1) 0)
+        first-play (<= timer-pos rec-len)
+        loop-trig (syn-if (or looped first-play) sync-trig 0)
+
+        env (asr attack sustain release)
+        env (env-gen:ar env sync-trig)
+
+        sig (scaled-play-buf:ar 1 in-buf :trigger loop-trig :loop 0)
+        sig (* env sig)]
+    (local-out:kr play-done)
+    (out:ar out-bus (pan2 sig))))
+
+
+(defmecha rp-player [& [in-buf 0
+                        out-bus 0
+                        timer-bus 1
+                        sync-bus -1
+                        looped true]]
+  (:start [play-node (syn-play in-buf
+                               out-bus
+                               timer-bus
+                               sync-bus
+                               (if looped 1 0))]
+          {:node play-node}))
+
+
+(defmecha rp [& [in-bus 0
+                 out-bus 0
+                 sync-bus -1
+                 looped true
+                 buf-size default-buffer-size
+                 attack 0.01
+                 sustain 1
+                 release 1]]
+  (:start [curr-rec (atom nil)
+           rec-mode (mecha (:start [r (rp-rec :in-bus in-bus
+                                              :sync-bus sync-bus
+                                              :buf-size buf-size)]
+                                   (reset! curr-rec r)
+                                   r))
+           play-mode (mecha (:start [p (if @curr-rec
+                                         (rp-player :in-buf (:out-buf @curr-rec)
+                                                    :out-bus out-bus
+                                                    :timer-bus (:timer-bus @curr-rec)
+                                                    :sync-bus sync-bus
+                                                    :looped looped)
+                                         nil)]
+                                    p))
+           modes (switch {:play play-mode
+                          :rec rec-mode})]
+          {:modes modes})
+  (:stop (-> @curr-rec :out-buf buffer-free)
+         (-> @curr-rec :timer-bus free-bus)))
+
+
+(defn rp-mode [t-rp mode-name]
+  ((:modes t-rp) mode-name))
--- test/lain/rp_test.clj
+++ test/lain/rp_test.clj
@@ -1,25 +1,154 @@
 (ns lain.rp-test
   (:require [speclj.core :refer :all]
-            [lain.mecha :as mecha]))
+            [overtone.libs.event :refer :all]
+            [overtone.algo.scaling :refer :all]
+            [overtone.sc.ugens :refer :all]
+            [overtone.sc.synth :refer :all]
+            [overtone.sc.bus :refer :all]
+            [overtone.sc.node :refer :all]
+            [lain.utils :refer [deflcgen]]
+            [lain.sequencers :refer [metro]]
+            [lain.mecha :as mecha]
+            [lain.rp :refer :all]))
+
+
+(defsynth syn-test-tick [bus 0
+                         freq 4]
+  (replace-out:ar bus (pulse-count:ar (impulse:ar freq))))
 
 
 (describe "rp"
   (with-stubs)
 
+  (after (sync-event :reset))
+
   (describe "rp"
-    (it "should support recording")
-    (it "should support playing")
-    (it "should support looped playing"))
+    (it "should record and play"
+      (let [in-bus (audio-bus)
+            out-bus (audio-bus)
+            m-out-bus (bus-monitor out-bus)
+            t-rp (rp :in-bus in-bus
+                     :out-bus out-bus
+                     :looped false)
+            input-node (syn-test-tick in-bus 4)]
+        (rp-mode t-rp :rec)
+        (Thread/sleep 1000)
+        (rp-mode t-rp :play)
 
-  (describe "modal-rp"
-    (describe "when the down key is pressed in recording mode"
-      (it "should start recording"))
+        ; no idea why 300 is working instead of 250 :/
+        ; maybe the monitor needs to catch up?
+        (Thread/sleep 300)
+        (should= 1.0 (round-to @m-out-bus 1))
 
-    (describe "when the mode key is released in recording mode"
-      (it "should start playing"))
+        (Thread/sleep 300)
+        (should= 2.0 (round-to @m-out-bus 1))
 
-    (describe "when the up key is pressed in play mode"
-      (it "should stop playing")))
+        (Thread/sleep 300)
+        (should= 3.0 (round-to @m-out-bus 1))
 
-    (describe "when the down key is pressed in play mode"
-      (it "should start playing")))
+        (Thread/sleep 300)
+        (should= 0.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 300)
+        (should= 0.0 (round-to @m-out-bus 1))
+
+        (kill input-node)
+        (free-bus in-bus)
+        (free-bus out-bus)
+        (mecha/stop t-rp)))
+
+    (it "should support synced recording and playing"
+      (let [in-bus (audio-bus)
+            out-bus (audio-bus)
+            sync-bus (control-bus)
+            m-out-bus (bus-monitor out-bus)
+            t-rp (rp :in-bus in-bus
+                     :out-bus out-bus
+                     :sync-bus sync-bus
+                     :looped false)
+            sync-node ((synth (out sync-bus (impulse 2))))
+            input-node (syn-test-tick in-bus 4)]
+        (rp-mode t-rp :rec)
+        (Thread/sleep 1500)
+
+        (rp-mode t-rp :play)
+        (Thread/sleep 450)
+        (should= 0.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 100)
+        (should= 2.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 300)
+        (should= 3.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 300)
+        (should= 4.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 500)
+        (should= 0.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 500)
+        (should= 0.0 (round-to @m-out-bus 1))
+
+        (kill input-node)
+        (free-bus in-bus)
+        (free-bus out-bus)
+        (free-bus sync-bus)
+        (mecha/stop t-rp)))
+
+    (it "should support looped playing"
+      (let [in-bus (audio-bus)
+            out-bus (audio-bus)
+            sync-bus (control-bus)
+            m-out-bus (bus-monitor out-bus)
+            t-rp (rp :in-bus in-bus
+                     :out-bus out-bus
+                     :sync-bus sync-bus
+                     :looped true)
+            sync-node ((synth (out sync-bus (impulse 2))))
+            input-node (syn-test-tick in-bus 4)]
+        (rp-mode t-rp :rec)
+        (Thread/sleep 1500)
+
+        (rp-mode t-rp :play)
+        (Thread/sleep 450)
+        (should= 0.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 100)
+        (should= 2.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 300)
+        (should= 3.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 300)
+        (should= 4.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 500)
+        (should= 0.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 500)
+        (should= 2.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 300)
+        (should= 3.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 300)
+        (should= 4.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 500)
+        (should= 0.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 300)
+        (should= 2.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 300)
+        (should= 3.0 (round-to @m-out-bus 1))
+
+        (Thread/sleep 300)
+        (should= 4.0 (round-to @m-out-bus 1))
+
+        (kill input-node)
+        (free-bus in-bus)
+        (free-bus out-bus)
+        (free-bus sync-bus)
+        (mecha/stop t-rp)))))
